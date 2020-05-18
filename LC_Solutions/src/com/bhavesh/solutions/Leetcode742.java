661d8ff9a858cd1fd151ef9a4490727d65e205bb
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-05-18 01:30:00 -0400

    Test Commit

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode742.java
@@ -0,0 +1,16 @@
+package com.bhavesh.solutions;
+
+public class Leetcode742 {
+
+	public static void main(String[] args) {
+		Solution742 s = new Solution742();
+	}
+}
+
+
+class Solution742{
+	public Solution742() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
