0bfac3f961a5eeaeda72e01598d4aa16a1cc1f4f
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-05-18 21:32:40 -0400

    TreeNode class Commit and Changes to 742

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode742.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode742.java
@@ -1,16 +1,55 @@
 package com.bhavesh.solutions;
 
 public class Leetcode742 {
-
-	public static void main(String[] args) {
-		Solution742 s = new Solution742();
-	}
-}
-
-
-class Solution742{
-	public Solution742() {
-		// TODO Auto-generated constructor stub
-	}
 	
+	int distance= Integer.MAX_VALUE;
+    int node = -1;
+    public int findClosestLeaf(TreeNode root, int k) {
+        findK(root, k);
+        return node;
+    }
+    
+    public int findK(TreeNode root, int k)
+    {
+        if(root == null)
+        {
+            return -1;
+        }
+        if(root.val == k)
+        {
+            findClosest(root, 0);
+            return 1;
+        }
+        int left = findK(root.left,k);
+        if(left > 0)
+        {
+            findClosest(root.right,++left);
+        }
+        int right = findK(root.right,k);
+        if(right > 0)
+        {
+            findClosest(root.left, ++right);
+        }
+        
+        return Math.max(left, right);
+    }
+    
+    public void findClosest(TreeNode root, int depth)
+    {
+        if(root == null)
+        {
+            return;
+        }
+        if(root.left ==null && root.right==null)
+        {
+            if(depth < distance)
+            {
+                distance = depth;
+                node = root.val;
+            }
+        }
+        findClosest(root.left,depth+1);
+        findClosest(root.right,depth+1);
+    }
 }
+
