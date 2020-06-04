0ff6e787a4a2624109cef1a24ba291317bf016a1
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-06-03 23:32:03 -0400

    Leetcode21

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode21.java
@@ -0,0 +1,27 @@
+package com.bhavesh.solutions;
+
+public class Leetcode21 {
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+		ListNode head = helper(l1, l2);
+		return head;
+	}
+
+	public ListNode helper(ListNode l1, ListNode l2) {
+
+		if (l1 == null) {
+			return l2;
+		}
+		if (l2 == null) {
+			return l1;
+		}
+
+		if (l1.val < l2.val) {
+			l1.next = helper(l1.next, l2);
+			return l1;
+		} else {
+			l2.next = helper(l1, l2.next);
+			return l2;
+		}
+
+	}
+}
