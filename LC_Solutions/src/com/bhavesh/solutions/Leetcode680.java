7861fbeaebd389cd9f41792cba61264110f1250a
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-06-08 21:41:27 -0400

    Leetcode680

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode680.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode680.java
@@ -1,5 +1,27 @@
 package com.bhavesh.solutions;
 
 public class Leetcode680 {
-
+	public boolean validPalindrome(String s) {
+        int i=0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return isValidPalindrome(s,i+1,j)|| isValidPalindrome(s,i,j-1);
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+    
+    public boolean isValidPalindrome(String s, int i, int j){
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
 }
