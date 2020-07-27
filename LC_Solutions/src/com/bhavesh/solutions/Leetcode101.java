155f225d6c878366e2b00d5b1f4d116f78fba07f
bhaveshagrawal <bhavesh.agrawal242@gmail.com>
2020-07-26 23:28:43 -0400

    Leetcode101

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode101.java
@@ -0,0 +1,21 @@
+package com.bhavesh.solutions;
+
+public class Leetcode101 {
+
+	public boolean isSymmetric(TreeNode root) {
+
+		return helper(root, root);
+	}
+
+	public boolean helper(TreeNode node1, TreeNode node2) {
+		if (node1 == null && node2 == null) {
+			return true;
+		}
+		if (node1 == null || node2 == null) {
+			return false;
+		}
+
+		return (node1.val == node2.val) && helper(node1.left, node2.right) && helper(node1.right, node2.left);
+
+	}
+}
