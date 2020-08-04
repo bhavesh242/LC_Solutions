d9f5a7e1fde3529f89648f187d85735a74b19c6d
bhaveshagrawal <bhavesh.agrawal242@gmail.com>
2020-08-04 01:28:17 -0400

    Leetcode238

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode238.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode238.java
@@ -1,5 +1,21 @@
 package com.bhavesh.solutions;
 
 public class Leetcode238 {
+	public int[] productExceptSelf(int[] nums) {
+		//Build a left array where left[0] = 1 and left[i] = product of elements nums[0] through nums[i-1]
+		int left[] = new int[nums.length];
+		left[0] = 1;
+		for (int i = 1; i < nums.length; i++) {
+			left[i] = left[i - 1] * nums[i - 1];
+		}
 
+		//now start doing the same process from right, left[i] = left[i] * (product nums[n] to nums[i+1]) exclusing nums[i]
+		int R = 1;
+		for (int j = nums.length - 1; j >= 0; j--) {
+			left[j] = left[j] * R;
+			R = R * nums[j];
+		}
+		//resultant array is solution
+		return left;
+	}
 }
