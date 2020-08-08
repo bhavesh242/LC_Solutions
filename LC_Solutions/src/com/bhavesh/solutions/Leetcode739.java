0e25eed3077a6aa2d467d9eda683a0b023cad749
bhaveshagrawal <bhavesh.agrawal242@gmail.com>
2020-08-07 21:52:48 -0400

    Comments added to Leetcode739

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode739.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode739.java
@@ -4,16 +4,24 @@ import java.util.Stack;
 
 public class Leetcode739 {
 	public int[] dailyTemperatures(int[] T) {
-        int sol[] = new int[T.length];
-        Stack<Integer> st = new Stack<Integer>();
-        for(int i=0;i<T.length;i++){
-            while(!st.empty() && T[st.peek()] < T[i])
-            {
-                int index = st.pop();
-                sol[index] = i - index;
-            }
-            st.push(i);
-            }   
-        return sol;
-    }
+		// Create an answer array
+		int ans[] = new int[T.length];
+		// Create a stack where we would store indexes
+		Stack<Integer> st = new Stack<Integer>();
+
+		for (int i = 0; i < T.length; i++) {
+			/*
+			 * If stack is not empty and the temperature at index at top of stack is smaller
+			 * than temperature at current index, it implies we are at a hotter day and pop
+			 * the stack index and put (index - current) at that index answer
+			 */
+			while (!st.isEmpty() && T[st.peek()] < T[i]) {
+				int index = st.pop();
+				ans[index] = i - index;
+			}
+			// Keep pushing current indexes on top of stack in search for hotter days
+			st.push(i);
+		}
+		return ans;
+	}
 }
