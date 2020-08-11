1101e7a9b8014405c700753f9d3defa33850374b
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-08-11 18:18:30 -0400

    Leetcode829

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode829.java
@@ -0,0 +1,21 @@
+package com.bhavesh.solutions;
+
+public class Leetcode829 {
+	public int consecutiveNumbersSum(int N) {
+		// from formula N = (x+1) + (x+2) + (x+2) ... + (x+k)
+
+		// k<= root(2N - 1/4) - 1/2
+		int upper_lim_k = (int) (Math.sqrt(2 * N + 0.25) - 0.5);
+		// Used to store the result
+		int count = 0;
+		for (int k = 1; k <= upper_lim_k; k++) {
+			// Also x is an interger meaning N/K - (k+1)/2 should also be an integer
+			// If that constraint is satisfied, increase count by 1
+			if ((N - k * (k + 1) / 2) % k == 0) {
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
