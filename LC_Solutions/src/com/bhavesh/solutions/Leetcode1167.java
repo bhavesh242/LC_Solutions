d27ee9a6c1b124ae571c8a6b44a2a2afe097acea
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-08-27 20:43:57 -0400

    Leetcode1167

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode1167.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1167.java
@@ -5,10 +5,12 @@ import java.util.PriorityQueue;
 public class Leetcode1167 {
 	public int connectSticks(int[] sticks) {
 		int sum = 0;
+		//Use a priority queeu to always select 2 smallest sticks
 		PriorityQueue<Integer> pq = new PriorityQueue();
 		for (int i = 0; i < sticks.length; i++) {
 			pq.add(sticks[i]);
 		}
+		//Until one stick remains, keep combinig smallest 2 sticks 
 		while (pq.size() > 1) {
 			int small1 = pq.remove();
 			int small2 = pq.remove();
