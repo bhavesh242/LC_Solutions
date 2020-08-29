1c40ee393e2106b9bec0c85b22b7b7e2c001cf53
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-08-29 00:14:59 -0400

    Leetcode139 with comments

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode139.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode139.java
@@ -5,19 +5,29 @@ import java.util.List;
 
 public class Leetcode139 {
 	public boolean wordBreak(String s, List<String> wordDict) {
-        int n = s.length();
-        boolean dp[] = new boolean[n+1];
-        HashSet<String> set = new HashSet<String>(wordDict);
-        dp[0] = true;
-        for(int i=1;i<=n;i++){
-            for(int j=0;j<i;j++){
-                if(dp[j] && set.contains(s.substring(j,i))){
-                    dp[i] = true;
-                    break;
-                }
-            }
-        }
-        
-        return dp[n];
-    }
+
+		// Convert the list of string into set for faster lookup
+		HashSet<String> set = new HashSet();
+		set.addAll(wordDict);
+
+		int n = s.length();
+
+		boolean dp[] = new boolean[n + 1];
+		// Dp of empty string "" is also a valid string, so dp[0] is true
+		dp[0] = true;
+		// Start traversing from index 1, i represents the index we are at on string s
+		for (int i = 1; i <= n; i++) {
+			// j checks for valid substring from 0 to i
+			for (int j = 0; j < i; j++) {
+				if (dp[j] && set.contains(s.substring(j, i))) {
+					dp[i] = true;
+					break;
+				}
+			}
+		}
+
+		// Result would be obtained at last index of dp array
+		return dp[n];
+
+	}
 }
