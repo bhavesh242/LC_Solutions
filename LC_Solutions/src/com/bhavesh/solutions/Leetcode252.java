6d032c14728861f3bb9e7e98e2a1bbb18970b536
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-09-03 18:52:13 -0400

    Leetcode252

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode252.java
@@ -0,0 +1,32 @@
+package com.bhavesh.solutions;
+
+import java.util.PriorityQueue;
+
+public class Leetcode252 {
+	public boolean canAttendMeetings(int[][] intervals) {
+		// Build a min heap that sorts by stating time
+		if (intervals.length == 0) {
+			return true;
+		}
+		PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a, b) -> (a[0] - b[0]));
+		// Add all intervals to priorityQueue
+		for (int a[] : intervals) {
+			pq.add(a);
+		}
+
+		// Take the smallest interval as the current interval
+		int[] cur = pq.remove();
+		// Remove intervals and keep checking if they overlap with current interval
+		// If yes, return false
+		while (!pq.isEmpty()) {
+			int[] next = pq.remove();
+			if (next[0] < cur[1]) {
+				return false;
+			}
+			cur = next;
+		}
+
+		return true;
+
+	}
+
