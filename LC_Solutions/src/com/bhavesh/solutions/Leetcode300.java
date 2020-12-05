9a4d7952a56c715db43175fa3a8aa82bee981891
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-12-04 23:41:44 -0500

    Leetcode 300

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode300.java
@@ -0,0 +1,22 @@
+package com.bhavesh.solutions;
+
+import java.util.Arrays;
+
+public class Leetcode300 {
+	public int lengthOfLIS(int[] nums) {
+		int[] dp = new int[nums.length];
+		int len = 0;
+		for (int num : nums) {
+			// Look at binary search documentation
+			int i = Arrays.binarySearch(dp, 0, len, num);
+			if (i < 0) {
+				i = -(i + 1);
+			}
+			dp[i] = num;
+			if (i == len) {
+				len++;
+			}
+		}
+		return len;
+	}
+}
