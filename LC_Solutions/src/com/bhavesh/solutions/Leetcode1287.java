cc7cf48092e0da995961d874ade1fd816ae500bb
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-12-06 22:53:43 -0500

    Leetcode 1287

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode1287.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1287.java
@@ -2,6 +2,7 @@ package com.bhavesh.solutions;
 
 public class Leetcode1287 {
 	public int findSpecialInteger(int[] arr) {
+		//Quart is to store 25% of the length
 		int quart = arr.length / 4;
 		for (int i = 0; i < arr.length - quart; i++) {
 			if (arr[i] == arr[i + quart]) {
