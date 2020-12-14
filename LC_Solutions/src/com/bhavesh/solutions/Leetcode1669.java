3f4132d4dbe93b8f535e5682990174b58b468b5a
bhavesh242 <bhavesh.agrawal242@gmail.com>
2020-12-13 22:11:39 -0500

    Leetcode 1669

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode1669.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1669.java
@@ -3,6 +3,10 @@ package com.bhavesh.solutions;
 public class Leetcode1669 {
 	public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
 
+		/*
+		 * Just need to attach a-1th node of list1 to head of list2 and tail of list 2
+		 * with b+1 th node of list1
+		 */
 		ListNode l2tail = list2;
 		while (l2tail.next != null) {
 			l2tail = l2tail.next;
