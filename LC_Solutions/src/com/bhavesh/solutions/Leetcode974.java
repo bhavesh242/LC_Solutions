007623f34d3eab897cbeb7f39cf81b7a19c288eb
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-02-03 14:49:36 -0500

    Leetcode974

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode974.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode974.java
@@ -2,8 +2,9 @@ package com.bhavesh.solutions;
 
 public class Leetcode974 {
 	public int subarraysDivByK(int[] A, int K) {
-		// Concept 1: If 2 prefix sums have same modulo, then all the indices between
-		// them sum upto divisible of K
+
+		// Concept : If 2 numbers return same modulo, then all rhe indices between them
+		// sum upto divisible of K
 		// https://www.youtube.com/watch?v=u9m-hnlcydk
 		int map[] = new int[K];
 		int sum = 0;
@@ -12,12 +13,16 @@ public class Leetcode974 {
 			sum += cur;
 			map[sum % K]++;
 		}
+		/*
+		 * Special Case for Prefix Sums that are divisible by K, i.e sum%K == 0 i.e
+		 * map[0]. For these cases, we need to consider the sums as well as the indexes
+		 * i.e if map[0] == n, then add n(n-1)/2 + n to the count
+		 */
 		int count = map[0];
 		for (int c : map) {
 			count += c * (c - 1) / 2;
 		}
 
 		return count;
-
 	}
 }
