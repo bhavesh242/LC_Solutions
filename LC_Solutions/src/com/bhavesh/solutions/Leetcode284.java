d4ced9928700ea6dce0402f9fecf35fd9b480675
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-02-08 15:16:04 -0500

    Leetcode 284

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode284.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode284.java
@@ -50,3 +50,47 @@ class PeekingIterator implements Iterator<Integer> {
 		return peeked != null || iterator.hasNext();
 	}
 }
+
+class PeekingIterator2 implements Iterator<Integer> {
+	Integer next;
+	Iterator<Integer> iter;
+
+	public PeekingIterator2(Iterator<Integer> iterator) {
+		// initialize any member here.
+		if (iterator.hasNext()) {
+			next = iterator.next();
+		}
+		iter = iterator;
+	}
+
+	// Returns the next element in the iteration without advancing the iterator.
+	public Integer peek() {
+		return next;
+	}
+
+	// hasNext() and next() should behave the same as in the Iterator interface.
+	// Override them if needed.
+	@Override
+	public Integer next() {
+		if (next == null) {
+			if (!iter.hasNext()) {
+				throw new NoSuchElementException();
+			}
+
+		}
+
+		Integer toReturn = next;
+		next = null;
+		if (iter.hasNext()) {
+			next = iter.next();
+		}
+
+		return toReturn;
+
+	}
+
+	@Override
+	public boolean hasNext() {
+		return next != null;
+	}
+}
