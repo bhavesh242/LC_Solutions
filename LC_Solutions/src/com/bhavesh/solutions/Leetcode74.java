3ccc1c70617ee286e1bbd66857217602e2238805
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-02-23 16:39:55 -0500

    Leetcode 74

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode74.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode74.java
@@ -2,26 +2,25 @@ package com.bhavesh.solutions;
 
 public class Leetcode74 {
 	public boolean searchMatrix(int[][] matrix, int target) {
-		// This is an array arranged as a 2D matrix, ranging from 0 to m*n-1
-		int row = matrix.length;
-		if (row == 0) {
-			return false;
-		}
-		int col = matrix[0].length;
-		if (col == 0) {
-			return false;
-		}
-		int left = 0, right = row * col - 1;
+
+		// Treat 2d array as a single long array
+
+		int R = matrix.length;
+		int C = matrix[0].length;
+
+		int left = 0;
+		int right = R * C - 1;
 		while (left < right) {
-			int mid = (right + left) / 2;
-			int ele = matrix[mid / col][mid % col];
-			if (ele >= target) {
-				right = mid;
-			} else {
+			int mid = left + (right - left) / 2;
+			int pRow = mid / C;
+			int pCol = mid % C;
+			if (matrix[pRow][pCol] < target) {
 				left = mid + 1;
+			} else {
+				right = mid;
 			}
 		}
 
-		return matrix[left / col][left % col] == target;
+		return matrix[left / C][left % C] == target;
 	}
 }
