ad61612281c64053c2d8ecaafc624b40a3828575
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-01 12:30:47 -0500

    Leetcode 1465

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1465.java
@@ -0,0 +1,31 @@
+package com.bhavesh.solutions;
+
+public class Leetcode1465 {
+	public int maxArea(int h, int w, int[] horizontalCuts, int[] verticalCuts) {
+
+		Arrays.sort(horizontalCuts);
+		Arrays.sort(verticalCuts);
+
+		int prevH = 0;
+		int maxH = 0;
+		for (int x : horizontalCuts) {
+			maxH = Math.max(maxH, x - prevH);
+			prevH = x;
+		}
+
+		maxH = Math.max(maxH, h - prevH);
+
+		int prevV = 0;
+		int maxV = 0;
+		for (int x : verticalCuts) {
+			maxV = Math.max(maxV, x - prevV);
+			prevV = x;
+		}
+
+		maxV = Math.max(maxV, w - prevV);
+
+		long l = 1L * maxH * maxV;
+		return (int) (l % 1000000007);
+
+	}
+}
