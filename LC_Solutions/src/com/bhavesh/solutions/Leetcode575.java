e4304ad3f28e8c3460c141f0a7092acfa8536b6b
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-01 22:59:40 -0500

    Leetcode 575

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode575.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode575.java
@@ -1,5 +1,15 @@
 package com.bhavesh.solutions;
 
-public class Leetcode575 {
+import java.util.HashSet;
 
+public class Leetcode575 {
+	public int distributeCandies(int[] candyType) {
+
+		HashSet<Integer> set = new HashSet<Integer>();
+		for (int x : candyType) {
+			set.add(x);
+		}
+
+		return Math.min(set.size(), candyType.length / 2);
+	}
 }
