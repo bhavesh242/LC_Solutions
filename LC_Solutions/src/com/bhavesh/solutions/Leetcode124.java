d4c58856a3ee738db407b4c80cd9247a43966f52
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-02 22:17:07 -0500

    Leetcode 124

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode124.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode124.java
@@ -21,8 +21,16 @@ public class Leetcode124 {
 		if (right > 0) {
 			val = val + right;
 		}
+		
+		//Check if the best path through this node is greater than current maxSum
 		maxSum = Math.max(maxSum, val);
-
+		
+		/*	If we are checking for other paths that pass through the parents of current node, then
+`			we can only consider one of the 2 subtrees as current node is the Lowest common ancestor for the 2 
+			subtrees so paths passing through ancestors of current node can include only one of the 2 subtrees
+		 	We naturally pass the maximal subtree to the ancestor, or just the node value if both subtrees are 
+		 	negative
+		 */
 		return Math.max(root.val, root.val + Math.max(left, right));
 	}
 }
