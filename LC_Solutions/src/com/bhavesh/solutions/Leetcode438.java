8777295f2b3b8143a6d3b3bd99e7587c1e796443
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-05 15:28:34 -0500

    Leetcode 438

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
@@ -4,42 +4,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode438 {
-	// Sliding Window Solution
+	
+	//Sliding window approach
+	
 	public List<Integer> findAnagrams(String s, String p) {
-		List<Integer> op = new ArrayList();
-		if (s.isEmpty() || p.isEmpty())
+
+		List<Integer> op = new ArrayList<Integer>();
+
+		if (p.length() == 0 || s.length() < p.length()) {
 			return op;
-		int map[] = new int[26];
-		int count = 0;
-		for (char c : p.toCharArray()) {
-			map[c - 'a']++;
 		}
-		int plen = p.length();
 
-		int left = 0, right = 0;
-		/*
-		 * Move right pointer always and keep a counter variable to check for charecters
-		 * present in string p
-		 */
+		int map[] = new int[26];
 
-		/*
-		 * Move left pointer until left and right pointer are placed at p.length()-1
-		 * distance
-		 */
-		/*
-		 * If all characters between these 2 pointers are present in string p, all left
-		 * pointer to output
-		 */
-		for (right = 0; right < s.length(); right++) {
-			if (--map[s.charAt(right) - 'a'] >= 0) {
-				count++;
+		for (char x : p.toCharArray()) {
+			map[x - 'a']++;
+		}
+
+		int left = 0;
+		for (int right = 0; right < s.length(); right++) {
+			map[s.charAt(right) - 'a']--;
+			while (map[s.charAt(right) - 'a'] < 0) {
+				map[s.charAt(left) - 'a']++;
+				left++;
 			}
-			while (right - left + 1 > plen) {
-				if (++map[s.charAt(left++) - 'a'] > 0) {
-					count--;
-				}
-			}
-			if (count == plen) {
+
+			if (right - left + 1 == p.length()) {
 				op.add(left);
 			}
 		}
