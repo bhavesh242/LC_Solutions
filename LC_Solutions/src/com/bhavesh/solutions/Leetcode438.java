6657a2f27b6849b86c820958c1711bc36dc2957b
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-08 10:45:40 -0500

    Leetcode 438

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
@@ -4,9 +4,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Leetcode438 {
-	
-	//Sliding window approach
-	
+
+	// Sliding window approach
+
 	public List<Integer> findAnagrams(String s, String p) {
 
 		List<Integer> op = new ArrayList<Integer>();
@@ -30,6 +30,40 @@ public class Leetcode438 {
 			}
 
 			if (right - left + 1 == p.length()) {
+				op.add(left);
+			}
+		}
+
+		return op;
+	}
+
+	//Approach 2 : Count Arrays
+	public List<Integer> findAnagrams_1(String s, String p) {
+
+		List<Integer> op = new ArrayList<Integer>();
+
+		if (p.length() == 0 || s.length() < p.length()) {
+			return op;
+		}
+
+		int map[] = new int[26];
+
+		for (char x : p.toCharArray()) {
+			map[x - 'a']++;
+		}
+
+		int left = 0;
+		int count = p.length();
+		for (int right = 0; right < s.length(); right++) {
+			if (right - left == p.length() && map[s.charAt(left++) - 'a']++ >= 0) {
+				count++;
+			}
+
+			if (--map[s.charAt(right) - 'a'] >= 0) {
+				count--;
+			}
+
+			if (count == 0) {
 				op.add(left);
 			}
 		}
