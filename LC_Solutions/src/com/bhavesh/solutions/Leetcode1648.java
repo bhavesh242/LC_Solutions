f837309df3050a440fba24d782b4064f0154412f
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-09 23:03:44 -0500

    Leetcode 1648

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode1648.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1648.java
@@ -4,15 +4,26 @@ import java.util.Arrays;
 
 public class Leetcode1648 {
 	public int maxProfit(int[] inventory, int orders) {
+
+		// We will greedily add most expensive balls to our profit
+
 		Arrays.sort(inventory);
 		int n = inventory.length - 1;
+
+		// Count is to maintain how many different colored balls at once we add to our
+		// order
 		int count = 1;
+
 		long ans = 0;
 		while (orders > 0) {
 			if (n > 0 && orders >= count * (inventory[n] - inventory[n - 1])) {
 				ans += count * getSum(inventory[n], inventory[n - 1]);
 				orders -= count * (inventory[n] - inventory[n - 1]);
-			} else {
+			}
+
+			// This is a special case where we need to complete remianer of order but need
+			// not add all the preceding balls to order as it is limited
+			else {
 				long a = orders / count;
 				ans += count * getSum(inventory[n], inventory[n] - a);
 				long b = orders % count;
