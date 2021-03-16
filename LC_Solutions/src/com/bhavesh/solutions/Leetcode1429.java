f7f397d437e2c7e66d7a5b9c1cddc9d530b24ae8
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-03-16 15:57:13 -0400

    Leetcode 1429

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode1429.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode1429.java
@@ -1,50 +1,40 @@
 package com.bhavesh.solutions;
 
 import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
 
 public class Leetcode1429 {
 
 }
 
 class FirstUnique {
-    Queue<Integer> queue = new LinkedList();
-    HashMap<Integer, Boolean> map = new HashMap();
-    public FirstUnique(int[] nums) {
-        //Add each element of the array to Queue
-        for(int num : nums){
-            add(num);
-        }
-    }
-    
-    public int showFirstUnique() {
-        //Remove all the elements from queue that are not unique
-        while(!queue.isEmpty() && !map.get(queue.peek())){
-            queue.remove();    
-        }
-        //If queue is empty, return -1 else return the first remaining element in queue
-        return queue.isEmpty()? -1 : queue.peek();
-    }
-    
-    
-    public void add(int value) {
-        /*If the map does not contain the value as key, add it to the queue 
-        and set unique to true*/
-        if(!map.containsKey(value)){
-            queue.offer(value);
-            map.put(value, true);
-        }
-        //Else the element is a duplicate, and set unique to false
-        else{
-            map.put(value, false);
-        }
-    }
-}
+	Map<Integer, Boolean> map;
+	Set<Integer> set;
 
-/**
- * Your FirstUnique object will be instantiated and called as such:
- * FirstUnique obj = new FirstUnique(nums);
- * int param_1 = obj.showFirstUnique();
- * obj.add(value);
- */
+	public FirstUnique(int[] nums) {
+		map = new HashMap<Integer, Boolean>();
+		set = new LinkedHashSet<Integer>();
+		for (int x : nums) {
+			add(x);
+		}
+	}
+
+	public int showFirstUnique() {
+		if (set.iterator().hasNext()) {
+			return set.iterator().next();
+		}
+		return -1;
+	}
+
+	public void add(int value) {
+		if (!map.containsKey(value)) {
+			map.put(value, true);
+			set.add(value);
+		} else {
+			map.put(value, false);
+			set.remove(value);
+		}
+	}
+}
