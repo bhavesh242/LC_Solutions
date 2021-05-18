933fd332798ac874657772b8a85d4481363274c6
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-05-18 15:51:46 -0400

    Leetcode 21

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode21.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode21.java
@@ -24,4 +24,30 @@ public class Leetcode21 {
 		}
 
 	}
+
+	public ListNode mergeTwoLists_iterative(ListNode l1, ListNode l2) {
+
+		ListNode head = new ListNode(-1);
+		ListNode cur = head;
+		while (l1 != null && l2 != null) {
+			if (l1.val < l2.val) {
+				cur.next = l1;
+				l1 = l1.next;
+			} else {
+				cur.next = l2;
+				l2 = l2.next;
+			}
+			cur = cur.next;
+		}
+
+		if (l1 != null) {
+			cur.next = l1;
+		}
+		if (l2 != null) {
+			cur.next = l2;
+		}
+
+		return head.next;
+	}
+
 }
