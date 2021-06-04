2a091c00078ee0e2781e01a8fa5788366a0a766c
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-04 19:40:53 -0400

    Leetcode 45

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode45.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode45.java
@@ -2,17 +2,43 @@ package com.bhavesh.solutions;
 
 public class Leetcode45 {
 	public int jump(int[] nums) {
-		int maxSteps = nums[0];
-		int maxPos = nums[0];
-		int numJumps = 1;
-		for (int i = 0; i < nums.length; i++) {
-			if (i > maxSteps) {
-				numJumps++;
-				maxSteps = Math.max(maxSteps, maxPos);
-			}
-			maxPos = Math.max(maxPos, i + nums[i]);
-		}
-		return numJumps;
 
+		/* We follow a greedy approach to get the minimum number of steps */
+		int jump = 0;
+		/*
+		 * This is to store what is the farthest we can go based based on what ladder we
+		 * climb based on all the the indices we have covered
+		 */
+		int farthest = 0;
+
+		/* This is to store how far we can go based on the current ladder we are on */
+
+		int currentJump = 0;
+
+		/*
+		 * Current jump and farthest is 0 because we start at 0th index and that is
+		 * farthest we can go initially
+		 */
+
+		/*
+		 * We iterate till the second last index only because we dont need to jump if we
+		 * reach the last index
+		 */
+		for (int i = 0; i < nums.length - 1; i++) {
+			// Look what is the farthest you can jump
+			farthest = Math.max(farthest, i + nums[i]);
+
+			/*
+			 * If we have reached to the end of our current jump we need to switch to
+			 * another far reaching ladder
+			 */
+			// Greedily, we jump to the ladder that reaches farthest
+			if (i == currentJump) {
+				jump++;
+				currentJump = farthest;
+			}
+		}
+
+		return jump;
 	}
 }
