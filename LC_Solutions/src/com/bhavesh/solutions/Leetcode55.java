02e95fe4a4bdc06c3aa14d0fadbf32510436c482
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-04 14:27:39 -0400

    Leetcode 55

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode55.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode55.java
@@ -1,8 +1,13 @@
 package com.bhavesh.solutions;
 
 public class Leetcode55 {
+
+}
+
+//Greedy Method
+class Sol55_1 {
 	public boolean canJump(int[] nums) {
-		if (nums.length == 1)
+		if (nums.length <= 1)
 			return true;
 		int maxWindow = nums[0];
 		int currentMax;
@@ -20,5 +25,40 @@ public class Leetcode55 {
 			}
 		}
 		return true;
+	}
+}
+
+//DFS with memoization
+enum Index {
+	GOOD, BAD, UNKNOWN
+}
+
+class Sol_55_2 {
+	Index[] memo;
+
+	public boolean canJumpFromPosition(int position, int[] nums) {
+		if (memo[position] != Index.UNKNOWN) {
+			return memo[position] == Index.GOOD ? true : false;
+		}
+
+		int furthestJump = Math.min(position + nums[position], nums.length - 1);
+		for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
+			if (canJumpFromPosition(nextPosition, nums)) {
+				memo[position] = Index.GOOD;
+				return true;
+			}
+		}
+
+		memo[position] = Index.BAD;
+		return false;
+	}
+
+	public boolean canJump(int[] nums) {
+		memo = new Index[nums.length];
+		for (int i = 0; i < memo.length; i++) {
+			memo[i] = Index.UNKNOWN;
+		}
+		memo[memo.length - 1] = Index.GOOD;
+		return canJumpFromPosition(0, nums);
 	}
 }
