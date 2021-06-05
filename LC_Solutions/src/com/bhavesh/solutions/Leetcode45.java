b7a6528ee9eb78c79232a776d3200c6f7cad3914
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-04 20:08:06 -0400

    Leetcode 45

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode45.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode45.java
@@ -1,5 +1,7 @@
 package com.bhavesh.solutions;
 
+import java.util.Arrays;
+
 public class Leetcode45 {
 	public int jump(int[] nums) {
 
@@ -40,5 +42,19 @@ public class Leetcode45 {
 		}
 
 		return jump;
+	}
+
+	// Solution 2 : DP (slower)
+	public int jump_2(int[] nums) {
+		int n = nums.length;
+		int[] dp = new int[n];
+		Arrays.fill(dp, Integer.MAX_VALUE);
+		dp[0] = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j <= i + nums[i] && j < n; j++) {
+				dp[j] = Math.min(dp[j], dp[i] + 1);
+			}
+		}
+		return dp[n - 1];
 	}
 }
