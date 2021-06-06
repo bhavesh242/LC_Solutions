9c98b84f25cd055a70f78254a5b8c08169a39821
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-06 13:13:27 -0400

    Leetcode 190

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode190.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode190.java
@@ -16,4 +16,19 @@ public class Leetcode190 {
 
 		return ans;
 	}
+
+	// Same solution as above but with a different representation
+	public int reverseBits_2(int n) {
+		int res = 0;
+		for (int i = 0; i < 32; i++) {
+			res += n & 1;// get the most right bit each time
+			n >>>= 1;// do UN-signed right shift by 1 each time
+			if (i < 31) {
+				res <<= 1;// shift this number to the left by 1 each time, so that eventually, this number
+							// is reversed
+			}
+		}
+		return res;
+	}
+
 }
