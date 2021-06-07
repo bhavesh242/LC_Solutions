daee5ab26079594444000e2b68a5720925af9961
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-07 02:27:19 -0400

    Leetcode 269

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode269.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode269.java
@@ -1,93 +1,113 @@
 package com.bhavesh.solutions;
 
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.LinkedList;
-import java.util.Map;
 import java.util.Queue;
 
 public class Leetcode269 {
 	public String alienOrder(String[] words) {
+		// create a graph using an arrays of HashSet of size 26 as we only deal with
+		// lowercase letters
+		HashSet<Character>[] graph = new HashSet[26];
+		// This array keeps track of the indegrees of characters, for topological sort
+		int[] indegree = new int[26];
+		// Fill indegree as -1 for all letters to keep track of letters we will never
+		// encounter
+		Arrays.fill(indegree, -1);
 
-		// create a graph using a hashmap
-		Map<Character, ArrayList<Character>> graph = new HashMap();
-
-		// This map keeps track of the indegrees of characters, for topological sort
-		Map<Character, Integer> indegree = new HashMap();
-
-		// Start Building a graph
-		// Firstly create a node for each character in the graph and assign it with
+		// Start Building the graph
+		// Firstly create a new hashset node for each character in the graph and assign
+		// it with
 		// indegree 0
+
+		// This variable keeps track of number of nodes in the graph
+		int graphSize = 0;
 		for (String word : words) {
 			for (char x : word.toCharArray()) {
-				graph.putIfAbsent(x, new ArrayList<Character>());
-				indegree.putIfAbsent(x, 0);
+				if (graph[x - 'a'] == null) {
+					graphSize++;
+					graph[x - 'a'] = new HashSet<Character>();
+					indegree[x - 'a'] = 0;
+				}
 			}
 		}
-
 		/*
 		 * Start travering consecutive words to build out a directed topological graph
 		 * the lexicographically smaller node serves as the parent and bigger serves as
 		 * a child
 		 */
 		for (int i = 0; i < words.length - 1; i++) {
-			String str1 = words[i];
-			String str2 = words[i + 1];
+			String a = words[i];
+			String b = words[i + 1];
 			/*
-			 * If the lexicographically smaller string is bigger in length and starts with
-			 * the second string, then the given arrangement is invalid and thus return an
-			 * empty string
+			 * If you encounter 2 words such that first word is larger and starts with the
+			 * second word, it implies the ordering is flawed and therefore retunr empty
+			 * string
 			 */
-			if (str1.length() > str2.length() && str1.startsWith(str2)) {
+			if (a.length() > b.length() && a.startsWith(b)) {
 				return "";
 			}
-			// else start constructing the graph
-			for (int j = 0; j < Math.min(str1.length(), str2.length()); j++) {
-				// Parent and Child character
-				char parent = str1.charAt(j);
-				char child = str2.charAt(j);
+
+			for (int j = 0; j < Math.min(a.length(), b.length()); j++) {
+				if (a.charAt(j) == b.charAt(j)) {
+					continue;
+				}
+				char child = b.charAt(j);
+				char parent = a.charAt(j);
+
+				// This commented section is a little check to see if the child already has and
+				// edge to parent
+				// It it does, they clearly form a cycle and return ""
+				// if(graph[child-'a'].contains(parent))
+				// {
+				// return "";
+				// }
+
 				/*
 				 * Add edge from parent to child character and increase indegree of child
 				 * character by one
 				 */
-				if (parent != child) {
-					if (!graph.get(parent).contains(child)) {
-						graph.get(parent).add(child);
-						indegree.put(child, indegree.get(child) + 1);
-					}
-					break;
+				if (!graph[parent - 'a'].contains(child)) {
+					graph[parent - 'a'].add(child);
+					indegree[child - 'a']++;
 				}
+				break;
 			}
 		}
+
+		// Result string
+		StringBuffer res = new StringBuffer();
+		Queue<Character> queue = new LinkedList<Character>();
 		/*
 		 * To start off our Topoligical sort bfs, start by adding all nodes with
 		 * indegree 0 to a queue
 		 */
-		Queue<Character> queue = new LinkedList();
-		for (char x : indegree.keySet()) {
-			if (indegree.get(x) == 0) {
-				queue.add(x);
+		for (char i = 'a'; i <= 'z'; i++) {
+			if (indegree[i - 'a'] == 0) {
+				queue.add(i);
 			}
 		}
-
-		// This stringbuffer wil store our result
-		StringBuffer op = new StringBuffer();
 		// Start off the BFS / Topological Sort
 		while (!queue.isEmpty()) {
-			char x = queue.remove();
-			op.append(x);
-			for (char child : graph.get(x)) {
-				indegree.put(child, indegree.get(child) - 1);
-				if (indegree.get(child) == 0) {
-					queue.offer(child);
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				char c = queue.poll();
+				res.append(c);
+				for (char x : graph[c - 'a']) {
+					indegree[x - 'a']--;
+					if (indegree[x - 'a'] == 0) {
+						queue.add(x);
+					}
 				}
 			}
 		}
+
 		/*
-		 * At the end of the sort we check if all the characters in the dictionary are
+		 * At the end of the sort we check if all the characters in the graph are
 		 * covered in the output string, if we have covered all then the alien
 		 * dictionary is valid else return an empty string
 		 */
-		return op.length() == graph.size() ? op.toString() : "";
+		return res.length() == graphSize ? res.toString() : "";
 	}
 }
