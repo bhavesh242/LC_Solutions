a67d0929627bca1d4121ed581f07d6dbe9ca96ab
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-10 18:25:57 -0400

    Leetcode 47

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode47.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode47.java
@@ -1,12 +1,14 @@
 package com.bhavesh.solutions;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Leetcode47 {
-	class Solution {
+
+	class Solution1 {
 		public List<List<Integer>> permuteUnique(int[] nums) {
 			List<List<Integer>> res = new ArrayList<>();
 			helper(nums, 0, nums.length, res);
@@ -40,4 +42,37 @@ public class Leetcode47 {
 			nums[r] = temp;
 		}
 	}
+
+	class Solution2 {
+		public List<List<Integer>> permuteUnique(int[] nums) {
+			List<List<Integer>> res = new ArrayList<List<Integer>>();
+			if (nums == null || nums.length == 0)
+				return res;
+			boolean[] used = new boolean[nums.length];
+			List<Integer> list = new ArrayList<Integer>();
+			Arrays.sort(nums);
+			dfs(nums, used, list, res);
+			return res;
+		}
+
+		public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res) {
+			if (list.size() == nums.length) {
+				res.add(new ArrayList<Integer>(list));
+				return;
+			}
+			for (int i = 0; i < nums.length; i++) {
+				if (used[i])
+					continue;
+				if (i > 0 && nums[i - 1] == nums[i] && !used[i - 1]) {
+					continue;
+				}
+				used[i] = true;
+				list.add(nums[i]);
+				dfs(nums, used, list, res);
+				used[i] = false;
+				list.remove(list.size() - 1);
+			}
+		}
+	}
+
 }
