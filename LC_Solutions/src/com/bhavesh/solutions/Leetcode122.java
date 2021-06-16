b5c118c545204ec03c507fbe1dabe7caf0295068
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-15 21:41:03 -0400

    Leetcode 122

---
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode122.java
@@ -0,0 +1,18 @@
+package com.bhavesh.solutions;
+
+public class Leetcode122 {
+	public int maxProfit(int[] prices) {
+		
+		//Intuition : Just add the sum of consecutive price differences everytime the price is more that previous. 
+		int prev = prices[0];
+		int sum = 0;
+		for (int i = 1; i < prices.length; i++) {
+			if (prices[i] > prev) {
+				sum += prices[i] - prev;
+			}
+			prev = prices[i];
+		}
+
+		return sum;
+	}
+}
