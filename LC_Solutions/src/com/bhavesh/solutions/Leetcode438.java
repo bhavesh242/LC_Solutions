5b29e6614d3e9fe6ae124f29f345e130a3dad747
bhavesh242 <bhavesh.agrawal242@gmail.com>
2021-06-16 15:47:44 -0400

    Leetcode 567

--- LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode438.java
@@ -6,68 +6,71 @@ import java.util.List;
 public class Leetcode438 {
 
 	// Sliding window approach
+	class Sol_438_1 {
+		public List<Integer> findAnagrams(String s, String p) {
 
-	public List<Integer> findAnagrams(String s, String p) {
+			List<Integer> op = new ArrayList<Integer>();
 
-		List<Integer> op = new ArrayList<Integer>();
+			if (p.length() == 0 || s.length() < p.length()) {
+				return op;
+			}
 
-		if (p.length() == 0 || s.length() < p.length()) {
+			int map[] = new int[26];
+
+			for (char x : p.toCharArray()) {
+				map[x - 'a']++;
+			}
+
+			int left = 0;
+			for (int right = 0; right < s.length(); right++) {
+				map[s.charAt(right) - 'a']--;
+				while (map[s.charAt(right) - 'a'] < 0) {
+					map[s.charAt(left) - 'a']++;
+					left++;
+				}
+
+				if (right - left + 1 == p.length()) {
+					op.add(left);
+				}
+			}
+
 			return op;
 		}
-
-		int map[] = new int[26];
-
-		for (char x : p.toCharArray()) {
-			map[x - 'a']++;
-		}
-
-		int left = 0;
-		for (int right = 0; right < s.length(); right++) {
-			map[s.charAt(right) - 'a']--;
-			while (map[s.charAt(right) - 'a'] < 0) {
-				map[s.charAt(left) - 'a']++;
-				left++;
-			}
-
-			if (right - left + 1 == p.length()) {
-				op.add(left);
-			}
-		}
-
-		return op;
 	}
 
-	//Approach 2 : Count Arrays
-	public List<Integer> findAnagrams_1(String s, String p) {
+	class Sol_438_2 {
+		// Approach 2 : Count Arrays
+		public List<Integer> findAnagrams_1(String s, String p) {
 
-		List<Integer> op = new ArrayList<Integer>();
+			List<Integer> op = new ArrayList<Integer>();
 
-		if (p.length() == 0 || s.length() < p.length()) {
+			if (p.length() == 0 || s.length() < p.length()) {
+				return op;
+			}
+
+			int map[] = new int[26];
+
+			for (char x : p.toCharArray()) {
+				map[x - 'a']++;
+			}
+
+			int left = 0;
+			int count = p.length();
+			for (int right = 0; right < s.length(); right++) {
+				if (right - left == p.length() && map[s.charAt(left++) - 'a']++ >= 0) {
+					count++;
+				}
+
+				if (--map[s.charAt(right) - 'a'] >= 0) {
+					count--;
+				}
+
+				if (count == 0) {
+					op.add(left);
+				}
+			}
+
 			return op;
 		}
-
-		int map[] = new int[26];
-
-		for (char x : p.toCharArray()) {
-			map[x - 'a']++;
-		}
-
-		int left = 0;
-		int count = p.length();
-		for (int right = 0; right < s.length(); right++) {
-			if (right - left == p.length() && map[s.charAt(left++) - 'a']++ >= 0) {
-				count++;
-			}
-
-			if (--map[s.charAt(right) - 'a'] >= 0) {
-				count--;
-			}
-
-			if (count == 0) {
-				op.add(left);
-			}
-		}
-
-		return op;
 	}
 }
--- LC_Solutions/src/com/bhavesh/solutions/Leetcode567.java
+++ LC_Solutions/src/com/bhavesh/solutions/Leetcode567.java
@@ -1,27 +1,56 @@
 package com.bhavesh.solutions;
 
 public class Leetcode567 {
-	public boolean checkInclusion(String s1, String s2) {
+	class Sol_567_1 {
+		public boolean checkInclusion(String s1, String s2) {
 
-		// Basic Sliding window, same as 438
-		int[] map = new int[26];
-		for (char x : s1.toCharArray()) {
-			map[x - 'a']++;
-		}
-
-		int left = 0;
-		for (int right = 0; right < s2.length(); right++) {
-			map[s2.charAt(right) - 'a']--;
-			while (map[s2.charAt(right) - 'a'] < 0) {
-				map[s2.charAt(left++) - 'a']++;
+			// Basic Sliding window, same as 438
+			int[] map = new int[26];
+			for (char x : s1.toCharArray()) {
+				map[x - 'a']++;
 			}
 
-			if (right - left + 1 == s1.length()) {
-				return true;
+			int left = 0;
+			for (int right = 0; right < s2.length(); right++) {
+				map[s2.charAt(right) - 'a']--;
+				while (map[s2.charAt(right) - 'a'] < 0) {
+					map[s2.charAt(left++) - 'a']++;
+				}
+
+				if (right - left + 1 == s1.length()) {
+					return true;
+				}
 			}
+
+			return false;
 		}
+	}
 
-		return false;
+	class Sol_567_2 {
+		public boolean checkInclusion(String s1, String s2) {
+			int map[] = new int[26];
+			for (int x : s1.toCharArray()) {
+				map[x - 'a']++;
+			}
+			int len = s1.length();
+			int left = 0;
+			int count = 0;
+			for (int right = 0; right < s2.length(); right++) {
+				if (map[s2.charAt(right) - 'a']-- > 0) {
+					count++;
+				}
+				while (right - left + 1 > len) {
+					if (map[s2.charAt(left++) - 'a']++ >= 0) {
+						count--;
+					}
+				}
 
+				if (count == len) {
+					return true;
+				}
+			}
+
+			return false;
+		}
 	}
 }
